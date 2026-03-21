fdbd7b43e1524d218687ee580e9eb20925b07db7
Denys Sychov <denys.sychov.dev@gmail.com>
2026-03-21 14:38:38 +0000

    feat: add initial setup for bank account sync feature with Docker configuration and Angular frontend

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'accounts',
+    loadChildren: () => import('./modules/bank-sync/bank-sync.module').then(m => m.BankSyncModule)
+  },
+  { path: '', redirectTo: '/accounts', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
