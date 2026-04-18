cc8e1075fbfa854705b6ef39e7c881c8158b562c
Denys Sychov <denys.sychov.dev@gmail.com>
2026-04-18 23:31:14 +0100

    [T010-T011] Add @types/google.accounts + GSI script; simplify config to ClientId only

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,4 +3,5 @@ export const environment = {
   apiBaseUrl: 'http://localhost:5050/api/v1',
   apiVersion: 'v1',
   wsUrl: 'ws://localhost:5050',
+  googleClientId: '687161855116-17f9guiugj8cdlat8h8c1vn5ji1irt8p.apps.googleusercontent.com',
 };
